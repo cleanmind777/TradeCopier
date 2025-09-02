238a6c73b8605033a46aadc114da748c3e74c801
cleanmind777 <cleanmind77777@gmail.com>
2025-09-02 19:05:34 -0400

    change: databse structure: add admin role to User table

--- backend/script/db.sql
+++ backend/script/db.sql
@@ -2,6 +2,7 @@ CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     email VARCHAR NOT NULL UNIQUE,
     name VARCHAR NOT NULL,
+    admin_role BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     is_verified BOOLEAN DEFAULT FALSE,
     otp_code VARCHAR,
