2ca34db278d4868f31ceb1828643461b4d581afd
cleanmind777 <cleanmind77777@gmail.com>
2025-09-10 05:50:01 -0400

    fix: update model

--- backend/app/models/broker_account.py
+++ backend/app/models/broker_account.py
@@ -29,6 +29,9 @@ class BrokerAccount(Base):
     expire_in = Column(String, nullable=True)
 
     user = relationship("User", back_populates="broker_accounts")
+    sub_broker_accounts = relationship(
+        "SubBrokerAccount", back_populates="broker_account"
+    )
 
 
 class SubBrokerAccount(Base):
