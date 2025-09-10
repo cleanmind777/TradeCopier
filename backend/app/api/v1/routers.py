b6f3754672ea3b2eff6c8ba5bd0b24142108fb0b
cleanmind777 <cleanmind77777@gmail.com>
2025-09-10 12:38:59 -0400

    fix: sub_broker endpoint

--- backend/app/api/v1/routers.py
+++ backend/app/api/v1/routers.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import auth, user, admin, broker, tradovate
+from app.api.v1.endpoints import auth, user, admin, broker, tradovate, sub_broker
 
 api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(user.router, prefix="/user", tags=["user"])
 api_router.include_router(admin.router, prefix="/admin", tags=["admin"])
 api_router.include_router(broker.router, prefix="/broker", tags=["broker"])
-api_router.include_router(broker.router, prefix="/subbroker", tags=["subbroker"])
+api_router.include_router(sub_broker.router, prefix="/subbroker", tags=["subbroker"])
 api_router.include_router(tradovate.router, prefix="/tradovate", tags=["tradovate"])
