c0815c929c5f858a24213bf211900d698bd87209
cleanmind777 <cleanmind77777@gmail.com>
2025-09-10 16:42:47 -0400

    fix: requirementsa and async db url

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ from app.services.broker_service import refresh_new_token
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from app.core.config import settings
 
-engine = create_async_engine(settings.DATABASE_URL, echo=True)
+engine = create_async_engine(settings.ASYNC_DATABASE_URL, echo=True)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
 
 
