af6cbe3378562cf153c5206bd488bfa32d2d1353
cleanmind777 <cleanmind77777@gmail.com>
2025-09-28 20:34:41 -0400

    fix: refresh time to 70minutes

--- backend/main.py
+++ backend/main.py
@@ -55,7 +55,7 @@ async def regenerate_access_token_periodically():
         async with async_session() as db:
             print("Regenerating access token...")
             await refresh_new_token(db)
-        await asyncio.sleep(4200)  # Sleep 2 minutes
+        await asyncio.sleep(4200)  # Sleep 70 minutes
 
 
 # FastAPI startup event to initialize DB and start background task
