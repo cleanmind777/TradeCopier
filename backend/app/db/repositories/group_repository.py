6d245280eab2b6143b681eed555f3b5d20c74b8d
cleanmind777 <cleanmind77777@gmail.com>
2025-10-03 14:03:52 -0700

    add: Set QTY to Group

--- backend/app/db/repositories/group_repository.py
+++ backend/app/db/repositories/group_repository.py
@@ -7,7 +7,7 @@ import secrets
 from datetime import datetime, timezone
 from uuid import UUID
 from sqlalchemy.future import select
-from app.schemas.group import GroupCreate, GroupNameChange, GroupAddBroker
+from app.schemas.group import GroupCreate, GroupNameChange, GroupAddBroker, GroupSetQTY
 from app.models.group import Group
 from app.models.group_broker import GroupBroker
 
@@ -50,3 +50,15 @@ def user_add_broker_to_group(db: Session, group_add_broker: GroupAddBroker):
     
 
     return db.query(GroupBroker).filter(GroupBroker.group_id==group_add_broker.group_id).all()
+
+def  user_set_qty_to_group(db: Session, group_set_qty: GroupSetQTY):
+    db_group = (
+        db.query(Group)
+        .filter(Group.id == group_set_qty.group_id)
+        .first()
+    )
+    db_group.qty = group_set_qty.qty
+    db.add(db_group)
+    db.commit()
+    db.refresh(db_group)
+    return db.query(Group).filter(Group.user_id==db_group.user_id).all()
--- backend/app/services/group_service.py
+++ backend/app/services/group_service.py
@@ -4,8 +4,8 @@ from fastapi import HTTPException, status, BackgroundTasks
 import json
 from uuid import UUID
 from datetime import datetime, date, time
-from app.schemas.group import GroupCreate, GroupNameChange, GroupAddBroker
-from app.db.repositories.group_repository import user_create_group, user_change_group_name, user_add_broker_to_group
+from app.schemas.group import GroupCreate, GroupNameChange, GroupAddBroker, GroupSetQTY
+from app.db.repositories.group_repository import user_create_group, user_change_group_name, user_add_broker_to_group, user_set_qty_to_group
 
 def create_group(db: Session, group_create: GroupCreate):
     return user_create_group(db, group_create)
@@ -15,3 +15,6 @@ def change_group_name(db: Session, change_name: GroupNameChange):
 
 def add_broker_to_group(db: Session, group_add_broker: GroupAddBroker):
     return user_add_broker_to_group(db, group_add_broker)
+
+def set_qty_to_group(db: Session, group_set_qty: GroupSetQTY):
+    return user_set_qty_to_group (db, group_set_qty)
