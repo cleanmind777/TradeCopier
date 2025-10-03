0792e97734663c8b858e16dbfb7aaff8dff2a459
cleanmind777 <cleanmind77777@gmail.com>
2025-10-03 13:23:14 -0700

    add: Add SubBroker to Group

--- backend/app/db/repositories/group_repository.py
+++ backend/app/db/repositories/group_repository.py
@@ -7,8 +7,9 @@ import secrets
 from datetime import datetime, timezone
 from uuid import UUID
 from sqlalchemy.future import select
-from app.schemas.group import GroupCreate, GroupNameChange
+from app.schemas.group import GroupCreate, GroupNameChange, GroupAddBroker
 from app.models.group import Group
+from app.models.group_broker import GroupBroker
 
 def user_create_group(
     db: Session, group_create: GroupCreate
@@ -33,3 +34,16 @@ def  user_change_group_name(db: Session, change_name: GroupNameChange):
     db.commit()
     db.refresh(db_group)
     return db.query(Group).filter(Group.user_id==db_group.user_id).all()
+
+def user_add_broker_to_group(db: Session, group_add_broker: GroupAddBroker):
+    for sub_broker in group_add_broker.sub_brokers:
+        db_group_broker = GroupBroker (
+            group_id = group_add_broker.group_id,
+            sub_broker_id = sub_broker
+        )
+        db.add(db_group_broker)
+        db.commit() 
+        db.refresh(db_group_broker)
+    
+
+    return db.query(GroupBroker).filter(GroupBroker.user_id==group_add_broker.group_id).all()
