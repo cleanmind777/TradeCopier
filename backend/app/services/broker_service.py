5a0716013f0142a60af954ad0d97644ddab10e56
cleanmind777 <cleanmind77777@gmail.com>
2025-10-06 02:38:35 -0700

    fix: await

--- backend/app/services/broker_service.py
+++ backend/app/services/broker_service.py
@@ -309,7 +309,7 @@ async def get_accounts(db: Session, user_id: UUID):
                 accounts_for_dashboard.append(a)
     return accounts_for_dashboard
 
-def get_sub_brokers_for_group(db: Session, user_id: UUID)->list[SubBrokerSumary]:
+async def get_sub_brokers_for_group(db: Session, user_id: UUID)->list[SubBrokerSumary]:
     db_broker_accounts = db.query(SubBrokerAccount).filter(
         SubBrokerAccount.user_id == user_id
     ).all()
