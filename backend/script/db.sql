e5964a1b90c372f48dfcb20957807785e057fb98
cleanmind777 <cleanmind77777@gmail.com>
2025-10-06 22:19:57 -0700

    Update: SQL

--- backend/script/db.sql
+++ backend/script/db.sql
@@ -45,12 +45,13 @@ CREATE TABLE groups (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id UUID NOT NULL REFERENCES users(id),
     name VARCHAR NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    qty INTEGER NOT NULL DEFAULT 0
-)
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    
+);
 
 CREATE TABLE groups_brokers (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     group_id UUID NOT NULL,
-    sub_broker_id UUID NOT NULL
-)
+    sub_broker_id UUID NOT NULL,
+    qty INTEGER NOT NULL DEFAULT 0
+);
