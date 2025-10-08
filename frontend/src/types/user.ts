8c44a479edba9e0d061fdd93f77d9fa2f7d00d1e
cleanmind777 <cleanmind77777@gmail.com>
2025-10-07 17:20:58 -0700

    Add: Upload Avatar

--- frontend/src/types/user.ts
+++ frontend/src/types/user.ts
@@ -1,12 +1,16 @@
+import { SrvRecord } from "dns";
+
 export interface UserCreate {
   email: string;
   name: string;
+  avatar?: string;
 }
 
 export interface User {
   id: string;
   email: string;
   name: string;
+  avatar: string | null;
   admin_role: boolean;
   is_accepted: boolean;
   created_at: Date;
