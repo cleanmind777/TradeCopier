8fb3ae8e5f860d8ddf3122468037aa43957b7eda
cleanmind777 <cleanmind77777@gmail.com>
2025-10-24 01:04:59 -0700

    fix: Endpoint

--- backend/app/api/v1/endpoints/databento.py
+++ backend/app/api/v1/endpoints/databento.py
@@ -8,9 +8,35 @@ from app.core.config import settings
 from app.services.databento_service import databento_price_stream
 from fastapi.responses import StreamingResponse
 from app.schemas.broker import Symbols
-
+import databento as db
+import asyncio, json
 router = APIRouter()
 
+# @router.post("/sse/current-price")
+# async def sse_endpoint(request: Request, data: Symbols):
+#     return StreamingResponse(databento_price_stream(request, data.symbols), media_type="text/event-stream")
+
 @router.post("/sse/current-price")
-async def sse_endpoint(request: Request, data: Symbols):
-    return StreamingResponse(databento_price_stream(request, data.symbols), media_type="text/event-stream")
+async def sse_current_price(request: Request, symbols_req: Symbols):
+    async def event_generator():
+        client = db.Live(key=settings.DATABENTO_KEY)
+        client.subscribe(
+            dataset="GLBX.MDP3",
+            schema="trades",
+            stype_in="parent",
+            symbols=symbols_req['symbols']
+        )
+        stream = client.start()
+
+        try:
+            for record in stream:
+                if await request.is_disconnected():
+                    break
+                data = record.asdict()
+                event_name = data.get("symbol", "message")
+                yield f"event: {event_name}\ndata: {json.dumps(data)}\n\n"
+                await asyncio.sleep(0)
+        except Exception as e:
+            print(f"Stream error: {e}")
+
+    return StreamingResponse(event_generator(), media_type="text/event-stream")
